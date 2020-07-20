b4945efd7e035c029de5965a689235c3f25e208c
amitlzkpa <amit.lzkpa@gmail.com>
2020-07-20 03:52:54 -0500

    query for rows in the board

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -4,23 +4,42 @@
       Hello {{ name }}
     </p>
     <button @click="onBtnClick">Click me</button>
+    <p v-for="row in rows">
+      {{ row.name }}
+    </p>
   </div>
 </template>
 
 <script>
+
+let ctx;
+
 export default {
   data () {
     return {
-      name: null
+      name: null,
+      rows: []
     };
   },
   async mounted () {
-    const res = await this.monday.api('query { me { name } }');
+    let res = await this.monday.api('query { me { name } }');
     this.name = res.data.me.name;
+
+    this.monday.listen("context", async (res) => {
+      ctx = res.data;
+    });
+
   },
   methods: {
     async onBtnClick () {
-      console.log('foo');
+      while(!ctx) await this.wait(200);
+
+      let boardId = ctx.boardId;
+      let queryStr = `query { boards (ids: ${boardId}) { name items { name } } }`;
+      console.log(queryStr);
+      let res = await this.monday.api(queryStr);
+      console.log(res.data);
+      this.rows = res.data.boards[0].items;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import mondaySdk from 'monday-sdk-js';
 
+Vue.prototype.wait = async function(ms) {
+	return new Promise((resolve) => setTimeout(() => resolve(), ms));
+}
+
 Vue.prototype.monday = mondaySdk();
 
 Vue.config.productionTip = false;
