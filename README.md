d5588509a065f72f618dbfc58ea870c870eff7eb
amitlzkpa <amit.lzkpa@gmail.com>
2020-07-23 04:19:57 -0500

    convert new project for requests view

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# dashboard
+# bidfriday-request
 
 ## Project setup
 ```
--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,17 +1,6 @@
 <template>
-  <div>
-    <p v-if="name !== null">
-      Hello {{ name }}
-    </p>
-
-    <br /><br />
-
-    <input type="text" v-model="newBoardName" />
-    <button @click="onNewBoardClk">New Request</button>
-
-    <br /><br />
-    
-    <button @click="onViewBoardClk">Show Board Items</button>
+  <div>    
+    <button @click="refresh">Refresh</button>
     <p v-for="row in rows" :key="row.name">
       {{ row.name }}
     </p>
@@ -25,72 +14,20 @@ let ctx;
 export default {
   data () {
     return {
-      name: null,
-      newBoardName: "My new request",
       rows: []
     };
   },
   async mounted () {
-    let res = await this.monday.api('query { me { name } }');
-    this.name = res.data.me.name;
 
     this.monday.listen("context", async (res) => {
       ctx = res.data;
     });
 
+    this.refresh();
+
   },
   methods: {
-    async onNewBoardClk() {
-      if(!this.newBoardName || this.newBoardName === "") return;
-      while(!ctx) await this.wait(200);
-
-      let mutStr;
-      let res;
-
-      mutStr = `mutation { create_board (board_name: "${this.newBoardName}", board_kind: private) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      let newBoardId = res.data.create_board.id;
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "Specifications", column_type: long_text) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "Units", column_type: dropdown) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "Quantity", column_type: numbers) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "Rate", column_type: numbers) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "Budget", column_type: numbers) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "People", column_type: people) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "Sample Images", column_type: integration) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "Attachments", column_type: integration) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-      mutStr = `mutation { create_column (board_id: ${newBoardId}, title: "Status", column_type: status) { id } }`;
-      res = await this.monday.api(mutStr);
-      console.log(res.data);
-
-    },
-    async onViewBoardClk() {
+    async refresh() {
       while(!ctx) await this.wait(200);
 
       let boardId = ctx.boardId;
